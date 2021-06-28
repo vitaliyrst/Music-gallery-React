62ab290ff88fbcb665fec184d97a359fac0f359a
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2021-06-29 00:42:06 +0300

    Update Members Page

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -1,38 +1,39 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect} from "react";
 import './Members.css';
 import {useDispatch, useSelector} from "react-redux";
 import {hideLoader, showLoader} from "../../redux/actions/actions";
 import Fallback from "../Loader/Loader";
 import {getGroupBiography, getLoading} from "../../redux/selectors";
+import aos from 'aos';
+import "aos/dist/aos.css";
 
 const Members = () => {
     const dispatch = useDispatch();
     const loading = useSelector(getLoading);
     const biography = useSelector(getGroupBiography);
-    const scrollRef = useRef();
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
+        aos.init({duration: 2000});
         biography ? dispatch(hideLoader()) : dispatch(showLoader());
     }, [dispatch, biography]);
 
-    const handleScroll = (eo) => {
-        console.log(eo.target)
-    }
     return (
         <>
             {loading ?
                 <Fallback className='loader' type='Puff' color='#c41818' width={150} height={150}/> :
-                <div className='members_container page' onScroll={handleScroll} ref={scrollRef}>
+                <div className='members_container page'>
                     <div className='members_list'>
                         {biography.map((member, index) => (
-                            <div key={member.id} className='members_list_item'>
+                            <div key={member.id}
+                                 data-aos={index === 0 ? 'fade-left' : index === 1 ? 'fade-right' : 'flip-left'}
+                                 className='members_list_item'
+                            >
                                 <div className='members_item_name'>{member.name}</div>
                                 <img className='members_item_image'
                                      style={{
                                          float: index % 2 === 0 ? 'right' : 'left',
                                          marginLeft: index % 2 !== 0 && '0',
-                                         marginRight: index % 2 === 0 &&'0'
+                                         marginRight: index % 2 === 0 && '0'
                                      }}
                                      src={member.image}
                                      alt={member.name}/>
